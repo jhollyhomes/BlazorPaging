ff3c80f482ff548892694b17f50608bc343140fa
James Hollyhomes <jamie.hollyhomes@hotmail.co.uk>
2024-01-02 15:57:26 +0000

    fixed page size issue, made it look a little better

--- src/BlazorPaging.Web/WebConstants.cs
+++ src/BlazorPaging.Web/WebConstants.cs
@@ -3,4 +3,7 @@
 public class WebConstants
 {
     public const string BlazorPagingApiName = "BlazorPagingApi";
+    public const int DefaultPageSize = 5;
+    public const string SortAscending = "Asc";
+    public const string SortDescending = "Desc";
 }
